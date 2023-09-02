8834894e855820f8796b6fce96fd3d1f9180c131
anttto <dosaeun@gmail.com>
2023-09-02 14:00:30 +0900

    type..

--- 1_type/1-2-function.ts
+++ 1_type/1-2-function.ts
@@ -1,2 +1,42 @@
 {
+    // function jsAdd(num1, num2) {
+    //     return num1 + num2;
+    // }
+
+    // function add(num1: number, num2: number): number {
+    //     return num1 + num2;
+    // }
+
+    // function jsFetchNum(id) {
+    //     //code...
+    //     //code...
+    //     return new Promise((resolve, reject) => {
+    //         resolve(100);
+    //     });
+    // }
+
+    // function jsFetchNum2(id: string): Promise<number> {
+    //     return new Promise((resolve, reject) => {
+    //         resolve(100);
+    //     });
+    // }
+
+    function printName(firstName: string, lastName?: string) {
+        console.log(firstName);
+        console.log(lastName);
+    }
+    printName('Steve', 'Jobs');
+    printName('Go');
+    printName('Awwl', undefined);
+
+    //Default parameter
+    function printMessage(message: string = 'default message') {
+        console.log(message);
+    }
+    printMessage();
+
+    function addNumber(...numbers: number[]): number {
+        return numbers.reduce((a, b) => a + b);
+    }
+    console.log(addNumber(3, 4));
 }
